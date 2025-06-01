8c8f58e41b57b463658ab2165f379e1bafbea3e6
mctuliomontoya <mctulio.2@gmail.com>
2025-05-31 20:53:43 -0700

    fix: change stats icons

--- src/components/Landing/sections/SectorStatsSection.tsx
+++ src/components/Landing/sections/SectorStatsSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as Progress from "@radix-ui/react-progress";
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { useEffect } from "react";
+import { TrendingUp } from "lucide-react";
 
 const AnimatedNumber = ({ value }: { value: number }) => {
   const motionValue = useMotionValue(0);
@@ -34,22 +35,16 @@ const SectorStatsSection: React.FC = () => (
         <span className="text-green-600 mb-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10"
             fill="none"
             viewBox="0 0 24 24"
+            strokeWidth={1.5}
             stroke="currentColor"
+            className="size-6"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth={2}
-              d="M3 21v-2a4 4 0 014-4h10a4 4 0 014 4v2"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M16 3.13a4 4 0 010 7.75M8 3.13a4 4 0 010 7.75"
+              d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5"
             />
           </svg>
         </span>
@@ -67,16 +62,16 @@ const SectorStatsSection: React.FC = () => (
         <span className="text-green-600 mb-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10"
             fill="none"
             viewBox="0 0 24 24"
+            strokeWidth={1.5}
             stroke="currentColor"
+            className="size-6"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 8c-1.657 0-3 1.343-3 3s1.343 3 3 3 3-1.343 3-3-1.343-3-3-3zm0 0V4m0 0C6.477 4 2 8.477 2 14c0 2.21 1.79 4 4 4h12c2.21 0 4-1.79 4-4 0-5.523-4.477-10-10-10z"
+              d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
             />
           </svg>
         </span>
@@ -94,16 +89,16 @@ const SectorStatsSection: React.FC = () => (
         <span className="text-green-600 mb-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10"
             fill="none"
             viewBox="0 0 24 24"
+            strokeWidth={1.5}
             stroke="currentColor"
+            className="size-6"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth={2}
-              d="M17 20h5v-2a4 4 0 00-4-4h-1M9 20H4v-2a4 4 0 014-4h1m0-4a4 4 0 118 0v4"
+              d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
             />
           </svg>
         </span>
@@ -119,20 +114,7 @@ const SectorStatsSection: React.FC = () => (
       </div>
       <div className="bg-white rounded-xl shadow-sm p-8 flex flex-col items-center border">
         <span className="text-green-600 mb-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M3 17l6-6 4 4 8-8"
-            />
-          </svg>
+          <TrendingUp className="size-6" />
         </span>
         <h3 className="text-xl font-bold mb-1 text-center">
           Crecimiento Anual
